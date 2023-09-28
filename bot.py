2f425f36126655154e25d10fcd70b4865730fde7
nowtiloos <nowtiloos@gmail.com>
2023-09-28 22:16:19 +0300

    initial

---
+++ bot.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config_data.config import load_config
+from handlers import other_handlers, user_handlers
+
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+
+    config = load_config('.env')
+
+    # Создаем объекты бота и диспетчера
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
